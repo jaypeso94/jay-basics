49be61283b2484f9751069be542c4c40ce3decdb
jaypeso94 <86980006+jaypeso94@users.noreply.github.com>
2021-08-09 17:13:01 -0400

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,32 @@
+set1 = {"message", "tag", "framework"}
+Variable1 = set1
+set1 = set(Variable1)
+
+set2 = {"message", "metadata", "token"}
+Variable2 = set2
+set2 = set(Variable2)
+
+set3 = {"metadata", "tag", "token"}
+Variable3 = set3   
+set3 = set(Variable3)
+
+#Trying to place all three into one variable
+# Then try the union approach but somethings off
+Set_All = {set1, set2, set3}
+
+def union(Set_All)
+print(union(Set_All))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
