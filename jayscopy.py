f6491be7499cf75e52a00a1e55614b2726018f3d
jaypeso94 <86980006+jaypeso94@users.noreply.github.com>
2021-08-28 16:54:03 -0400

    Add files via upload

---
+++ jayscopy.py
@@ -0,0 +1,18 @@
+#Practice
+
+#asd = {"message", "tag", "framework"}
+#vwv = {"message", "metadata", "token", "ping", "nack"}
+#weve = {"metadata", "tag", "token"}
+#ewr = {"framework", "message", "echo", "token"}
+#set_list = [asd, vwv, weve, ewr]
+
+#def sort_list_of_sets(set_list):
+#	sorted_list = sort_list_of_sets(set_list)
+
+#print(sorted_list)
+
+
+for i in range(len(set_list)):
+        set_1 = set_list[i]
+    if set_list[i] > set_list[i + 1]:
+
