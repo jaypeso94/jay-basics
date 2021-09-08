b61a885317967008748430a3f3a5c4c7260a3433
jaypeso94 <86980006+jaypeso94@users.noreply.github.com>
2021-09-08 00:48:58 -0400

    Add files via upload

---
+++ SortingFunction.py
@@ -0,0 +1,11 @@
+listA = [2, 0, 1, 3, 7, 4, 6, 9, 8]
+listB = [19, 12, 76, 43, 23, 15, 54, 28]
+
+listA.sort()
+print(listA)
+
+listB.sort()
+print(listB)
+
+#Not sure if numbers were meant to be included but
+#I figured you were looking to start with something simple
