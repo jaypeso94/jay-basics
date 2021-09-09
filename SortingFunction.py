bb3ad21cc93c68de8359313e62689a86b9c3f064
jaypeso94 <86980006+jaypeso94@users.noreply.github.com>
2021-09-09 01:20:20 -0400

    Update SortingFunction.py

--- SortingFunction.py
+++ SortingFunction.py
@@ -1,11 +1,15 @@
 listA = [2, 0, 1, 3, 7, 4, 6, 9, 8]
 listB = [19, 12, 76, 43, 23, 15, 54, 28]
 
-listA.sort()
-print(listA)
+def listAB(listA, listB):
+	listA.sort()
+	listB.sort()
 
-listB.sort()
-print(listB)
+	listC = (listA, listB)
+	return listC
+
+listABC = listAB(listA, listB)
+print(listABC)	
 
 #Not sure if numbers were meant to be included but
 #I figured you were looking to start with something simple
